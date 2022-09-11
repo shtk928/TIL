3d7c318030931c80cf79ee3a0a95be4dd5bb1eae
shtk928 <sh.tk928@gmail.com>
2022-09-11 16:00:42 +0900

    create react_count_app

--- React/React_App/react_count_app/src/App.js
+++ React/React_App/react_count_app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import Button from './Button';
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Box'>
+        <p>カウント： {count}</p>
+        <Button btn_click={() => { setCount((prevCount) => prevCount + 1) }} btn_txt='プラスする' />
+        <Button btn_click={() => { setCount((prevCount) => prevCount - 1) }} btn_txt='マイナスする'/>
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ React/React_App/react_count_app/src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button(props) {
+  return (
+    <button onClick={props.btn_click}>
+      {props.btn_txt}
+    </button>
+  )
+}
+
+export default Button
