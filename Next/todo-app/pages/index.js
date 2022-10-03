1ed093421af8c962453a0a8cc4c358f6d2f07cc5
shtk928 <sh.tk928@gmail.com>
2022-10-03 14:50:40 +0900

    todo edit function create

--- Next/todo-app/pages/index.js
+++ Next/todo-app/pages/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 import db from '../src/firebase';
-import { collection, deleteDoc, doc, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, onSnapshot, serverTimestamp, updateDoc } from "firebase/firestore";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
   const [isEdit, setIsEdit] = useState(false);
+  const [edittingTodo, setEdittingTodo] = useState({});
+  const [editTodoId, setEditTodoId] = useState('');
 
   // useEffectを使用 リロード時に、一度だけデータを取得
   useEffect(() => {
@@ -22,11 +24,32 @@ const Home = () => {
 
   const onClickEdit = (editTodo) => {
     setIsEdit(true);
+    setEdittingTodo({title: editTodo.data().title, status: editTodo.data().status});
+    setEditTodoId(editTodo.id);
+  }
+
+  const onClickEditComplete = async (editTodo) => {
+    await updateDoc(doc(db, 'todos', `${editTodo.id}`), {
+      title: edittingTodo.title,
+      status: edittingTodo.status,
+      timestamp: serverTimestamp()
+    });
+    setIsEdit(false);
+    setEditTodoId('');
+  }
+
+  const onClickEditCancel = () => {
+    setIsEdit(false);
+    setEditTodoId('');
   }
 
   const onClickDelete = async (deleteTodo) => {
     await deleteDoc(doc(db, 'todos', `${deleteTodo.id}`));
   }
+
+  useEffect(() => {
+    console.log(edittingTodo);
+  }, [edittingTodo])
 
   return (
     <>
@@ -36,9 +59,25 @@ const Home = () => {
           {todos?.map((todo) => {
             return (
               <ul key={todo.id}>
-                <li>title : {todo.data().title}</li>
-                <li>status : {todo.data().status ? '完了' : '未完了'}</li>
-                <button type='button'>編集</button>
+                {isEdit && todo.id === editTodoId ? 
+                  <>
+                    <form>
+                      <label>title : </label>
+                      <input type='text' value={edittingTodo.title} onChange={(e) => setEdittingTodo({...edittingTodo, title: e.target.value})} />
+                      <br/>
+                      <label>status : </label>
+                      <button type='button' onClick={() => setEdittingTodo({...edittingTodo, status: !edittingTodo.status})}>{edittingTodo.status ? '完了' : '未完了'}</button>
+                    </form>
+                    <button type='button' onClick={() => onClickEditComplete(todo)}>完了</button>
+                    <button type='button' onClick={() => onClickEditCancel()}>キャンセル</button>
+                  </>
+                  : 
+                  <>
+                    <li>title : {todo.data().title}</li>
+                    <li>status : {todo.data().status ? '完了' : '未完了'}</li>
+                    <button type='button' onClick={() => onClickEdit(todo)}>編集</button>
+                  </>
+                }
                 <button type='button' onClick={() => onClickDelete(todo)}>削除</button>
               </ul>
             )
