4473bba9acca98a05448b85812de6ab646cbb67a
shtk928 <s.takanori0119@icloud.com>
2022-11-26 03:21:29 +0900

    Google Map API

--- GoogleMapsPlatform/maps_javascripts_api/pages/index.tsx
+++ GoogleMapsPlatform/maps_javascripts_api/pages/index.tsx
@@ -1,6 +1,20 @@
-export default function Home() {
+import * as React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const Home = () => {
+  const center = React.useMemo(() => ({ lat: 35.681382, lng: 139.766084 }), []);
+
   return (
-    <>
-    </>
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}>
+      <GoogleMap
+        zoom={12}
+        center={center}
+        mapContainerClassName='map-container'
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
   )
 }
+
+export default Home;
